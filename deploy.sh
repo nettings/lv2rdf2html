19afc789d9132081b32feb974ab91a46cc814014
Jörn Nettingsmeier <nettings@stackingdwarves.net>
2017-08-28 20:32:03 +0200

    make script more intelligent and cleanly killable without leaving stray notifiers behind.

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,55 @@
-#!/bin/sh
+#!/bin/bash
 SERVER=86.83.27.183
 DOCROOT=/srv/www/htdocs/
+SOURCE=~/output.xml
+SLEEPTIME=5
+
+function deployHTML {
 while : ; do  
   inotifywait -e modify lv2rdf2html.xsl iterators.xsl gui-*.xsl ;
-  xsltproc lv2rdf2html.xsl ~/Desktop/*media*/output.xml \
+  xsltproc lv2rdf2html.xsl "$SOURCE" \
     | xsltproc xml-prettyprint.xsl - \
-    | ssh $SERVER "cat > $DOCROOT/index.html"
-  sleep 5
-done &
+    | ssh $SERVER "cat > '$DOCROOT'/index.html"
+  sleep $SLEEPTIME
+done
+}
 
+function deployPHP {
 while : ; do
   inotifywait -e modify lv2rdf2php.xsl ;
-  xsltproc lv2rdf2php.xsl ~/Desktop/*media*/output.xml \
-    | ssh $SERVER "cat > $DOCROOT/pluginController.php"
-  sleep 5
-done &
+  xsltproc lv2rdf2php.xsl $SOURCE \
+    | ssh $SERVER "cat > '$DOCROOT'/pluginController.php"
+  sleep $SLEEPTIME
+done
+}
 
+function deployOther {
 while : ; do
   inotifywait -e modify *css *js ;
-  scp *css *js $SERVER:$DOCROOT
-  sleep 5
+  scp *css *js $SERVER:"$DOCROOT"
+  sleep $SLEEPTIME
 done
+}
+  
+deployHTML &
+deployHTML_PID="$!"
+deployPHP &
+deployPHP_PID="$!"
+deployOther &
+deployOther_PID="$!"
 
-  
-  
+function user_break {
+  echo "Ok, preparing to exit..."
+  kill $deployHTML_PID
+  kill $deployPHP_PID
+  kill $deployOther_PID
+  echo "Cleaned up, goodbye!"
+  exit 0;
+}
+
+trap user_break SIGINT
+
+while : ; do
+  echo "Hit Control-C to cleanly terminate $0." 
+  sleep 60
+done
