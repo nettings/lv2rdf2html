380c382b63ec0829d571ea2c20752d232b4de9db
Jörn Nettingsmeier <nettings@stackingdwarves.net>
2017-11-29 00:08:29 +0100

    now deploys all files once upon startup before setting watchers.

--- deploy.sh
+++ deploy.sh
@@ -1,50 +1,72 @@
 #!/bin/bash
-SERVER=86.83.27.183
-DOCROOT=/srv/www/htdocs/
+SERVER=localhost
+DOCROOT=/var/www/html/
 SOURCE=~/output.xml
 SLEEPTIME=5
 
-echo "Hit Control-C to terminate $0." 
 
 function deployHTML {
-while : ; do  
-  inotifywait -e modify lv2rdf2html.xsl iterators.xsl gui-*.xsl ;
   xsltproc lv2rdf2html.xsl "$SOURCE" \
     | xsltproc xml-prettyprint.xsl - \
     | ssh $SERVER "cat > '$DOCROOT'/index.html"
+
+}
+
+function watchHTML {
+while : ; do  
+  inotifywait -e modify lv2rdf2html.xsl iterators.xsl gui-*.xsl "$SOURCE";
+  deployHTML
   sleep $SLEEPTIME
 done
 }
 
 function deployPHP {
-while : ; do
-  inotifywait -e modify lv2rdf2php.xsl ;
-  xsltproc lv2rdf2php.xsl $SOURCE \
+  xsltproc lv2rdf2php.xsl "$SOURCE" \
     | ssh $SERVER "cat > '$DOCROOT'/pluginController.php"
+}
+
+function watchPHP {
+while : ; do
+  inotifywait -e modify lv2rdf2php.xsl "$SOURCE";
+  deployPHP
   sleep $SLEEPTIME
 done
 }
 
 function deployOther {
+  scp *css *js $SERVER:"$DOCROOT"
+}
+
+function watchOther {
 while : ; do
   inotifywait -e modify *css *js ;
-  scp *css *js $SERVER:"$DOCROOT"
+  deployOther
   sleep $SLEEPTIME
 done
 }
+
+echo "Initial deployment of all files:"
+echo -n "HTML code... "
+deployHTML && echo "done." || echo "failed."  
+echo -n "PHP code... "
+deployPHP && echo "done." || echo "failed."
+echo  "CSS and Javascript... "  
+deployOther && echo "done." || echo "failed."  
+
+echo "Hit Control-C to terminate $0." 
   
-deployHTML &
-deployHTML_PID="$!"
-deployPHP &
-deployPHP_PID="$!"
-deployOther &
-deployOther_PID="$!"
+watchHTML &
+watchHTML_PID="$!"
+watchPHP &
+watchPHP_PID="$!"
+watchOther &
+watchOther_PID="$!"
 
 function user_break {
   echo "Ok, preparing to exit..."
-  kill $deployHTML_PID
-  kill $deployPHP_PID
-  kill $deployOther_PID
+  kill $watchHTML_PID
+  kill $watchPHP_PID
+  kill $watchOther_PID
   echo "Cleaned up, goodbye!"
   exit 0;
 }
